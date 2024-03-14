d5c4100688e5df730ffd228f5bf0a2c5520a6787
dmarts05 <dmarts05@estudiantes.unileon.es>
2024-03-14 16:21:53 +0100

    feat: add selection by roulette

--- internal/selection/selection.go
+++ internal/selection/selection.go
@@ -1,6 +1,8 @@
 package selection
 
 import (
+	"math/rand/v2"
+
 	"github.com/dmarts05/genetic-n-queens/internal/individual"
 	"github.com/dmarts05/genetic-n-queens/internal/util"
 )
@@ -17,6 +19,37 @@ func SelectByTournament(population []*individual.Individual) []*individual.Indiv
 			selected = append(selected, ind1)
 		} else {
 			selected = append(selected, ind2)
+		}
+	}
+
+	return selected
+}
+
+func SelectByRoulette(population []*individual.Individual) []*individual.Individual {
+	selected := []*individual.Individual{}
+	totalFitness := 0
+	for _, ind := range population {
+		totalFitness += ind.Fitness()
+	}
+
+	probabilities := make([]float64, len(population))
+	for i, ind := range population {
+		probabilities[i] = float64(ind.Fitness()) / float64(totalFitness)
+	}
+
+	cummulative_probabilities := make([]float64, len(population))
+	cummulative_probabilities[0] = probabilities[0]
+	for i := 1; i < len(population); i++ {
+		cummulative_probabilities[i] = cummulative_probabilities[i-1] + probabilities[i]
+	}
+
+	for len(selected) < len(population) {
+		r := rand.Float64()
+		for i, p := range cummulative_probabilities {
+			if r <= p {
+				selected = append(selected, population[i])
+				break
+			}
 		}
 	}
 
